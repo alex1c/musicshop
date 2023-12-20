c90c87265892e055ac79bebd6894ba14fa8a1b30
alex1c <alex1c-spb@yandex.ru>
2023-12-20 20:34:49 +0300

    commit 20122023

--- app/admin/upload/page.jsx
+++ app/admin/upload/page.jsx
@@ -1,28 +1,75 @@
-import Footer from '@/components/Footer'
-import Menu from '@/components/Menu'
-import React from 'react'
+'use client'
+
+import Footer from "@/components/Footer";
+import Menu from "@/components/Menu";
+import React, { useState, useEffect } from "react";
+import { FormEvent } from 'react'
 
 const upload = () => {
-  return (
-    <>upload
-    <Menu></Menu>
+    const [domLoaded, setDomLoaded] = useState(false);
 
-    <div className='mt-14'><form action="/api/catalog">
-
-<button type='submit'></button>
-
-</form></div>
-
-<form action="/api/catalog">
-
-<button type="submit" class="ml-14 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload price</button>
-
-</form>
+    // useEffect(() => {
+    //     setDomLoaded(true);
+    // }, []);
 
 
-    <Footer></Footer>
-    </>
-  )
-}
+    async function onSubmit(FormEvent) {
+      FormEvent.preventDefault()
+   
+      const formData = new FormData(FormEvent.currentTarget)
+      const response = await fetch('/api/catalog', {
+        method: 'GET',
+       // body: formData,
+      })
+   
+      // Handle response if necessary
+     // const data = await response.json()
+      // ...
+    }
 
-export default upload
+    async function onSubmitDelete(FormEvent) {
+      FormEvent.preventDefault()
+   
+      const formData = new FormData(FormEvent.currentTarget)
+      const response = await fetch('/api/catalog', {
+        method: 'DELETE',
+        //body: formData,
+      })
+   
+      // Handle response if necessary
+     // const data = await response.json()
+      // ...
+    }
+
+    return (
+      
+        <div>
+          
+            <Menu />
+            
+            <div className="flex justify-between mx-14 mt-72">
+            <form  onSubmit={onSubmit}>
+                <button
+                    type="submit"
+                    class="ml-14 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                    Upload price
+                </button>
+            </form>
+            <form  onSubmit={onSubmitDelete}>
+                <button
+                    type="submit"
+                    class="ml-14 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                >
+                    Delete price
+                </button>
+            </form>
+            </div>
+            <Footer />
+            
+        </div>
+      
+    );
+};
+
+export default upload;
--- app/api/catalog/route.js
+++ app/api/catalog/route.js
@@ -1,21 +1,91 @@
+'use client'
+
 import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
 //import price from '../../price/lutner_new.csv'
+import { redirect } from "next/navigation";
+import { useRouter } from 'next/navigation'
+
+async function handler(arrayOfObjectForMongoDB) {
+    const client = await MongoClient.connect(
+        "mongodb+srv://alex1cspb:4VWWdH31VLw6SN62@lutner.vfkxvei.mongodb.net/lutnerdatabase"
+    );
+    const db = client.db();
+    const collection = db.collection("pricecollection");
+
+    //delete
+    if (arrayOfObjectForMongoDB.length === 0) {
+       // try {
+            const result = await collection.deleteMany({});
+            return result;
+       // } catch (e) {
+         //   console.log("Ошибка при удалении ", e);
+       // }
+    } else {
+        // const result = await collection.insertOne({heading, description, done})
+        const result = await collection.insertMany(arrayOfObjectForMongoDB);
+        return result;
+    }
+
+    //insert
+    //try { //
+    // const client = await MongoClient.connect(
+    //     "mongodb+srv://alex1cspb:4VWWdH31VLw6SN62@lutner.vfkxvei.mongodb.net/lutnerdatabase"
+    // );
+    // const db = client.db();
+    // const collection = db.collection("pricecollection");
+    // // const result = await collection.insertOne({heading, description, done})
+    // const result = await collection.insertMany(arrayOfObjectForMongoDB);
+    //const result = await collection.deleteMany({})
+    //console.log('result --------- ',typeof result)
+    //return result
+
+    //  } catch (error) {
+    // console.log('error --------- ',error)
+    //  return redirect('/admin/upload')
+    //  return error;
+    //  }
+}
+
+export async function DELETE(Request) {
+    console.log("Request---", Request);
+
+    const router = useRouter()
+
+    let arrayOfObjectForMongoDB = new Array();
+
+    // let res = handler(arrayOfObjectForMongoDB).then((value) => {
+    //     console.log("value----", value).then(redirect(`/admin/upload?p=${value}`));
+    let res = handler(arrayOfObjectForMongoDB).then((value) => {
+        router.push(`/admin/upload?p=${value}`);
+        //здесь { acknowledged: true, deletedCount: 0 }
+    });
+    // .then(redirect(`/admin/upload?p=${value}`));
+
+    // console.log('result---',res)
+
+    return NextResponse.json({ message: "dat" });
+}
 
 export async function GET(Request) {
-    let price;
-    var myHeaders = new Headers();
+    // var myHeaders = new Headers();
+    console.log("Request---", Request);
 
-    myHeaders.append("Content-Type", "text/plain; charset=windows 1251");
-
+    // myHeaders.append("Content-Type", "text/plain; charset=windows 1251");
+    //читаем прайс в локальном каталоге. Он должен быть переведен в кодировку UTF8
+    //заголовки не работали
+    // не забывай добавлять текущий ип в монго
     //fetch('https://lutner.ru/bitrix/catalog_export/upload/lutner_new.csv')
-    fetch("http://localhost:3000/lutner_new.csv", myHeaders, {
+    fetch("http://localhost:3000/lutner_new.csv", {
         cache: "no-store",
     })
         .then((response) => response.text())
-        .then((data) => parse(data)); //console.log(data));
+        .then((data) => parse(data));
+    // .then(console.log(data)); //console.log(data));
 
     // console.log(data)
 
+    //разбиваем прайс сначала построчно и потом каждую строку кладем в массив по точке с запятой
     function parse(price) {
         //console.log(price);
 
@@ -25,14 +95,81 @@ export async function GET(Request) {
         //for (let i = 0; i < lines.length; i++) {
         for (let i = 0; i < 200; i++) {
             csvData[i] = lines[i].split(";");
+            //console.log("---lenth ", csvData[i].length);
         }
 
-        console.log("---", csvData[5]);
+        let arrayOfObjectForMongoDB = new Array();
+
+        for (let k = 0; k < csvData.length - 1; k++) {
+            //for (let k = 0; k < 200; k++) {
+            if (csvData[k].length == 41) {
+                for (let t = 0; t < csvData[k].length; t++) {
+                    const priceString = {
+                        _id: csvData[t][0],
+                        IE_NAME: csvData[t][1],
+                        IE_PREVIEW_TEXT: csvData[t][2],
+                        IE_DETAIL_PICTURE:
+                            "http://www.lutner.ru" + csvData[t][3],
+                        CP_QUANTITY: csvData[t][4], //Остаток
+                        IP_PROP134: csvData[t][5], //Количество струн
+                        IP_PROP140: csvData[t][6], //Актуальность товара (Снят с производства)
+                        IP_PROP113: csvData[t][7], //Тип губной гармошки
+                        IP_PROP112: csvData[t][8], //Количество частей у блок-флейт
+                        IP_PROP114: csvData[t][9], //Производитель
+                        IP_PROP111: csvData[t][10], //Тип блок-флейты
+                        IP_PROP121: csvData[t][11], //Цвет1
+                        IP_PROP96: csvData[t][12], //Артикул
+                        IP_PROP99: csvData[t][13], //Единица измерения
+                        IP_PROP127: csvData[t][14], //Не используется
+                        IP_PROP100: csvData[t][15], //Ставка НДС
+                        IP_PROP97: csvData[t][16], //Не используется
+                        IP_PROP95: csvData[t][17], //Штрих код
+                        IP_PROP122: csvData[t][18], //Верхняя дека
+                        IP_PROP123: csvData[t][19], //Тональность
+                        IP_PROP149: csvData[t][20], //Не используется
+                        IP_PROP106: csvData[t][21], //вес брутто
+                        IP_PROP107: csvData[t][22], //Объем
+                        IP_PROP165: csvData[t][23], //Мощность
+                        IP_PROP110: csvData[t][24], //Высота звучания
+                        IC_GROUP0: csvData[t][25], //Группа1
+                        IC_GROUP1: csvData[t][26], //Группа2
+                        IC_GROUP2: csvData[t][27], //Группа3
+                        IP_PROP131: csvData[t][28], //Размер
+                        IP_PROP142: csvData[t][29], //Ширина верхнего порожка
+                        IP_PROP153: csvData[t][30], //Диаметр
+                        IP_PROP139: csvData[t][31], //Цвет2
+                        CV_PRICE_13: csvData[t][32], //Цена дилер
+                        IP_PROP152: csvData[t][33], //Материал обмотки
+                        CV_CURRENCY_13: csvData[t][34], //Валюта
+                        IP_PROP176: csvData[t][35], //Хит продаж
+                        IP_PROP150: csvData[t][36], //Диаметр первой струны
+                        CV_PRICE_18: csvData[t][37], //Цена розница
+                        CV_CURRENCY_18: csvData[t][38], //Валюта
+                        CV_PRICE_20: csvData[t][39], //Цена МП
+                        CV_CURRENCY_20: csvData[t][40], //Валюта
+                    };
+                    arrayOfObjectForMongoDB.push(priceString);
+
+                    // console.log("priceString--", priceString);
+                }
+            }
+        }
+
+        // console.log("---", csvData);
+        // console.log("---lenth ", csvData.length);
+        // console.log("arrayOfObjectForMongoDB ---", arrayOfObjectForMongoDB);
+
+        //const req = {heading: 'head', description: 'desc'}
+
+        handler(arrayOfObjectForMongoDB);
+        //res.then((res) => res.json())
+        //console.log("result-----", res);
+        // console.log("---", dat);
     }
 
-    
+    //сделать возврат результата на вызывающую страницу
+    // redirect(`/admin/upload?p=${dat}`);
+    redirect("/admin/upload");
 
-    // console.log("---", csvData);
-
-    return NextResponse.json({ message: csvData });
+    return NextResponse.json({ message: "dat" });
 }
